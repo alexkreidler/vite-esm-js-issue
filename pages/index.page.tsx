0aa74ef0f9c4d250f95e1681216e53f85ee3bb6d
Your Name <you@example.com>
2021-11-13 21:42:00 -0500

    create example

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-// import { Counter } from "./index/Counter";
+
+import * as schema from "compassql/build/src/schema";
+
+import covidData from "../data.json"
+
+const { build, Schema } = schema
+
+// This is an overly simplified example that doesn't do much itself,
+// in fact it may not work at runtime. But simply getting it to compile, run and then emit an error would be great!
+const covidSchema = new Schema(covidData.schema as any)
 
 export { Page };
 
 function Page() {
   return (
     <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive.
-        </li>
-      </ul>
+    <h1>Hello world</h1>
+    {covidSchema.tableSchema().fields.map((f) => <p>{f.name}</p>)}
     </>
   );
 }
